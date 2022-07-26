67aefb87d36aa8e54ed68cfe9474fb3fd16160e3
Matias Sotelo <48103401+matisotee@users.noreply.github.com>
2022-07-26 03:45:10 -0300

    [MINOR] Stocks API (#3)

--- stock_market/urls.py
+++ stock_market/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from stock_market.controllers.sign_up import SignUpAPIView
+from stock_market.controllers.stock_market import StockMarketAPIView
 
 urlpatterns = [
     path('sign_up/', SignUpAPIView.as_view()),
+    path('stocks/<str:symbol>', StockMarketAPIView.as_view()),
 ]
