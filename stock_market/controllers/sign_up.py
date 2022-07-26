9fd664d02ebc827e43d0dfa6cad44d7bad871fce
Matias Sotelo <48103401+matisotee@users.noreply.github.com>
2022-07-26 00:26:31 -0300

    [MINOR] Signup API (#2)

---
+++ stock_market/controllers/sign_up.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import generics
+from rest_framework.exceptions import APIException
+
+from rest_framework.permissions import AllowAny
+
+from stock_market.services.sign_up import sign_up, IncorrectPasswordError
+
+
+class SignUpSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        required=True,
+    )
+    name = serializers.CharField(
+        required=False,
+    )
+    last_name = serializers.CharField(
+        required=False,
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    api_key = serializers.CharField(
+        read_only=True,
+    )
+
+    def create(self, validated_data):
+        try:
+            response = sign_up(
+                email=validated_data['email'],
+                password=validated_data['password'],
+                name=validated_data['name'],
+                last_name=validated_data['last_name']
+            )
+        except IncorrectPasswordError:
+            raise APIException("INCORRECT_PASSWORD")
+
+        return response
+
+
+class SignUpAPIView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = SignUpSerializer
--- stock_market/settings.py
+++ stock_market/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'stock_market',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'stock_market.User'
